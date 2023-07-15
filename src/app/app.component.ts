2592f01604db7112a3c043c3dde5d6c184072e4f
Daly <dalyna64@gmail.com>
2023-07-15 16:38:41 +0100

    http service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { UserService } from './services/user.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'loginApp';
+  constructor(private userService: UserService) {}
+  ngOnInit() {
+    this.userService.get();
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,4 +8,9 @@ import { catchError, retry } from 'rxjs/operators';
 })
 export class UserService {
   constructor(private http: HttpClient) {}
+
+  get() {
+    let users = this.http.get('https://loginapp-api-ra2l.onrender.com/users');
+    users.subscribe((data) => console.log(data));
+  }
 }

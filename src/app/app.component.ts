6679d359ff1c9664624ce9c33dececb2a29890ea
Daly <dalyna64@gmail.com>
2023-07-18 23:52:24 +0100

    tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {User} from './models/User';
-import {UserService} from './services/user.service';
-import {subscribeOn} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/User';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +10,14 @@ import {subscribeOn} from "rxjs";
 export class AppComponent implements OnInit {
   title = 'loginApp';
   users: User[] = [];
-  user: User = new User()
+  user: User = new User();
 
-  constructor(private userService: UserService) {
-  }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
     this.userService.getAll().subscribe((users) => (this.users = users));
-    this.userService.getOn('64b1af383689c6f83f595baa').subscribe((user) => (this.user=user))
+    this.userService
+      .getOn('64b1af383689c6f83f595baa')
+      .subscribe((user) => (this.user = user));
   }
 }

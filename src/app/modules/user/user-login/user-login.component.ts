d2fadd6e1609433ec156b4875659c7cb4d40d90c
Daly <dalyna64@gmail.com>
2023-07-24 20:06:56 +0100

    remouve toaster and bootstrap 2006-24072023

--- src/app/modules/user/user-login/user-login.component.ts
+++ src/app/modules/user/user-login/user-login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../models/User';
 import { UserService } from '../services/user.service';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user-login',
@@ -10,44 +9,30 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./user-login.component.css'],
 })
 export class UserLoginComponent implements OnInit {
-  constructor(
-    private userService: UserService,
-    private router: Router,
-    private toastr: ToastrService
-  ) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   title = 'User Login Page';
-  baseURL: String = 'https://loginapp-api-ra2l.onrender.com/';
   user: User = new User();
   testUser: User = new User();
   responce: String = '';
 
   ngOnInit(): void {
-    this.userService.getOn('64be9acc1951b1e4ded8a683').subscribe({
+    this.userService.getOn('64beca17ef980cff64c42fa1').subscribe({
       next: (user: User) => (this.testUser = user),
       error: (error: any) => {
         throw error;
       },
     });
-    console.log(this.testUser);
-  }
-
-  imageurl() {
-    if (this.testUser.image)
-      return this.baseURL + 'getUserImage/' + this.user.image;
-    else return '';
   }
 
   login() {
     this.userService.login(this.user).subscribe({
       next: (data) => {
         this.responce = data.message;
-        this.toastr.success(this.responce.trim(), 'success');
         this.router.navigate(['detail']);
       },
       error: (error) => {
         this.responce = error.error.message;
-        this.toastr.error(this.responce.trim());
       },
     });
   }
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserLoginComponent } from './user-login/user-login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { UserRoutingModule } from './user-routing.module';
 import { UserDetailComponent } from './user-detail/user-detail.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRoutingModule } from './user-routing.module';
 @NgModule({
   declarations: [UserLoginComponent, UserDetailComponent],
   imports: [
@@ -14,14 +12,7 @@ import { ToastrModule } from 'ngx-toastr';
     CommonModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule, // required animations module
-    ToastrModule.forRoot({
-      timeOut: 7000,
-      positionClass: 'toast-top-right',
-      preventDuplicates: true,
-      progressBar: true,
-      progressAnimation: 'increasing',
-    }), // ToastrModule added
+   
   ],
 })
 export class UserModule {}
